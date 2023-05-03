2cb442d29c4f40bfc5fe82f3e02e556d7f5fe64c
Gabriel Torres <gaabrieltt7@gmail.com>
2023-05-03 13:27:13 -0300

    test: should be able to authenticate

---
+++ src/use-cases/authenticate.spec.ts
@@ -0,0 +1,25 @@
+import { it, expect, describe } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { AuthenticateUseCase } from './authenticate'
+import { hash } from 'bcryptjs'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+
+describe('Authenticate useCase', () => {
+  it('should be able to authenticate', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateUseCase(usersRepository)
+
+    await usersRepository.create({
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password_hash: await hash('valid_password', 6)
+    })
+
+    const { user } = await sut.execute({
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    })
+
+    expect(user).toHaveProperty('id')
+  })
+})
