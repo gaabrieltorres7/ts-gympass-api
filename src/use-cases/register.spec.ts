8f7ca7c761f3cbdfabcf14bbe304ec055f5e43a1
Gabriel Torres <gaabrieltt7@gmail.com>
2023-05-02 21:13:58 -0300

    test: should not be able to register with same email twice

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -2,6 +2,7 @@ import { it, expect, describe } from 'vitest'
 import { RegisterUseCase } from './register'
 import { compare } from 'bcryptjs';
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository';
+import { UserAlreadyExistsError } from './errors/user-already-exists-error';
 
 describe('Register useCase', () => {
   it('should hash user password upon registration', async () => {
@@ -19,4 +20,24 @@ describe('Register useCase', () => {
     expect(isPasswordHashed).toBe(true)
 
   }) 
+
+  it('should not be able to register with same email twice', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const email = 'same_email@mail.com'
+
+    await registerUseCase.execute({
+      name: 'any_name',
+      email: email,
+      password: 'valid_password'
+    })
+
+    expect(() => registerUseCase.execute({
+      name: 'any_name',
+      email: email,
+      password: 'valid_password'
+    })).rejects.toBeInstanceOf(UserAlreadyExistsError)
+
+  })
 })
