43fa3267b483dab9b5fde051e8f49aef2458d582
Gabriel Torres <gaabrieltt7@gmail.com>
2023-05-04 21:48:18 -0300

    test: should be able to get user profile

---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,31 @@
+import { it, expect, describe, beforeEach } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { hash } from 'bcryptjs'
+import { GetUserProfileUseCase } from './get-user-profile'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+let usersRepository: InMemoryUsersRepository
+let sut: GetUserProfileUseCase
+
+describe('Get User Profile useCase', () => {
+
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileUseCase(usersRepository)
+  })
+    
+  it('should be able to get user profile', async () => {
+    const createdUser = await usersRepository.create({
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password_hash: await hash('valid_password', 6)
+    })
+
+    const { user } = await sut.execute({
+      userId: createdUser.id
+    })
+
+    expect(user).toHaveProperty('id')
+    expect(user).toHaveProperty('name')
+  })
+})
