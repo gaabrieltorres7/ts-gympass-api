ad13748abba994576610fdb8b4fea3d6f0d3182e
Gabriel Torres <gaabrieltt7@gmail.com>
2023-05-06 15:52:55 -0300

    feat: ensure check in is not created on the same day

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -14,6 +14,10 @@ export class CheckInUseCase {
   constructor(private CheckInsRepository: CheckInsRepository) {}
 
   async execute({ userId, gymId }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const checkInOnSameDate = await this.CheckInsRepository.findByUserIdOnDate(userId, new Date())
+
+    if(checkInOnSameDate) throw new Error()
+
     const checkIn = await this.CheckInsRepository.create({
       gym_id: gymId,
       user_id: userId
