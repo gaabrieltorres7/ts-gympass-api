616e600bc0b8d87c115b1630321f409781d05329
Gabriel Torres <gaabrieltt7@gmail.com>
2023-05-06 15:50:09 -0300

    feat: add findByUserIdOnDate in CheckInsRepository's interface

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -1,4 +1,5 @@
 import { Prisma, CheckIn } from '@prisma/client'
 export interface CheckInsRepository {
   create(checkIn: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+  findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
 }
