0f00ebb863d14c30a4382672a73d6adc2efc58a9
Gabriel Torres <gaabrieltt7@gmail.com>
2023-05-08 21:28:24 -0300

    test: should be able to search for gyms

---
+++ src/use-cases/search-gyms.spec.ts
@@ -0,0 +1,39 @@
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { expect, describe, it, beforeEach } from 'vitest'
+import { SearchGymsUseCase } from './search-gyms'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: SearchGymsUseCase
+
+describe('Search Gyms Use Case', () => {
+  beforeEach(async () => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new SearchGymsUseCase(gymsRepository)
+  })
+
+  it('should be able to search for gyms', async () => {
+    await gymsRepository.create({
+      title: 'JavaScript Gym',
+      description: null,
+      phone: null,
+      latitude: -27.2092052,
+      longitude: -49.6401091,
+    })
+
+    await gymsRepository.create({
+      title: 'TypeScript Gym',
+      description: null,
+      phone: null,
+      latitude: -27.2092052,
+      longitude: -49.6401091,
+    })
+
+    const { gyms } = await sut.execute({
+      query: 'JavaScript',
+      page: 1,
+    })
+
+    expect(gyms).toHaveLength(1)
+    expect(gyms).toEqual([expect.objectContaining({ title: 'JavaScript Gym' })])
+  })
+})
