be900fb2227717bb5cbc2b0ab782cf0804676fc7
Gabriel Torres <gaabrieltt7@gmail.com>
2023-05-12 20:26:43 -0300

    chore: test environment config

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,9 @@
+import { Environment } from "vitest";
+
+export default <Environment> {
+  name: "prisma",
+  async setup() {
+    console.log('setup')
+    return { teardown() { console.log('teardown')} }
+  }
+}
