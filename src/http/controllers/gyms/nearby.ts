3450c0d121e84a53f6d5eaf960d1869f965ee840
Gabriel Torres <gaabrieltt7@gmail.com>
2023-05-13 11:37:22 -0300

    feat: implement gyms routes

--- src/http/controllers/gyms/nearby.ts
+++ src/http/controllers/gyms/nearby.ts
@@ -2,7 +2,7 @@ import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
 import { makeFetchNearbyGymsUseCase } from '@/use-cases/factories/make-fetch-nearby-gyms-use-case';
 
-export async function create(req: FastifyRequest, res: FastifyReply) {
+export async function nearby(req: FastifyRequest, res: FastifyReply) {
   const nearbyGymsQuerySchema = z.object({
     latitude: z.number().refine((value) => Math.abs(value) <= 90, { message: 'Latitude must be between -90 and 90' }),
     longitude: z.number().refine((value) => Math.abs(value) <= 180, { message: 'Longitude must be between -180 and 180' }),
