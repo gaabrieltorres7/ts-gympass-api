98a5a81912931ef65b47887638ab4a520d697a64
Gabriel Torres <gaabrieltt7@gmail.com>
2023-05-13 11:35:21 -0300

    feat: fetch nearby gyms controller

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,20 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { makeFetchNearbyGymsUseCase } from '@/use-cases/factories/make-fetch-nearby-gyms-use-case';
+
+export async function create(req: FastifyRequest, res: FastifyReply) {
+  const nearbyGymsQuerySchema = z.object({
+    latitude: z.number().refine((value) => Math.abs(value) <= 90, { message: 'Latitude must be between -90 and 90' }),
+    longitude: z.number().refine((value) => Math.abs(value) <= 180, { message: 'Longitude must be between -180 and 180' }),
+  })
+
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(req.body)
+
+  const fetchNearbyGymsUseCase = makeFetchNearbyGymsUseCase()
+
+  const { gyms } = await fetchNearbyGymsUseCase.execute({ userLatitude: latitude, userLongitude: longitude })
+
+  return res.status(201).send({ gyms })
+
+}
+
