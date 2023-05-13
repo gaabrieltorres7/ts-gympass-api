dc2e24f3f6ca0e80fc0ae9a06510c6dc7fa8641e
Gabriel Torres <gaabrieltt7@gmail.com>
2023-05-13 12:48:18 -0300

    test: nearby gyms e2e

--- src/http/controllers/gyms/nearby.ts
+++ src/http/controllers/gyms/nearby.ts
@@ -4,8 +4,8 @@ import { makeFetchNearbyGymsUseCase } from '@/use-cases/factories/make-fetch-nea
 
 export async function nearby(req: FastifyRequest, res: FastifyReply) {
   const nearbyGymsQuerySchema = z.object({
-    latitude: z.number().refine((value) => Math.abs(value) <= 90, { message: 'Latitude must be between -90 and 90' }),
-    longitude: z.number().refine((value) => Math.abs(value) <= 180, { message: 'Longitude must be between -180 and 180' }),
+    latitude: z.coerce.number().refine((value) => Math.abs(value) <= 90, { message: 'Latitude must be between -90 and 90' }),
+    longitude: z.coerce.number().refine((value) => Math.abs(value) <= 180, { message: 'Longitude must be between -180 and 180' }),
   })
 
   const { latitude, longitude } = nearbyGymsQuerySchema.parse(req.query)
@@ -14,7 +14,7 @@ export async function nearby(req: FastifyRequest, res: FastifyReply) {
 
   const { gyms } = await fetchNearbyGymsUseCase.execute({ userLatitude: latitude, userLongitude: longitude })
 
-  return res.status(201).send({ gyms })
+  return res.status(200).send({ gyms })
 
 }
 
