bff7595fd333e207b6810b956e486f47ac04c334
Gabriel Torres <gaabrieltt7@gmail.com>
2023-05-13 11:27:24 -0300

    feat: create gym controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
-import { appRoutes } from './http/routes'
+import { usersRoutes } from './http/controllers/users/routes'
+import { gymsRoutes } from './http/controllers/gyms/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
@@ -10,7 +11,10 @@ app.register(fastifyJwt, {
   secret: env.JWT_SECRET
 })
 
-app.register(appRoutes)
+app.register(usersRoutes)
+app.register(gymsRoutes)
+
+
 app.setErrorHandler((error, req, res) => {
   if(error instanceof ZodError) {
     return res.status(400).send({ message: 'Validation failed', issues: error.format() })
---
+++ src/http/controllers/gyms/create.ts
@@ -0,0 +1,24 @@
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error';
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { makeCreateGymUseCase } from '@/use-cases/factories/make-create-gym-use-case';
+
+export async function create(req: FastifyRequest, res: FastifyReply) {
+  const createGymBodySchema = z.object({
+    title: z.string(),
+    description: z.string().nullable(),
+    phone: z.string().nullable(),
+    latitude: z.number().refine((value) => Math.abs(value) <= 90, { message: 'Latitude must be between -90 and 90' }),
+    longitude: z.number().refine((value) => Math.abs(value) <= 180, { message: 'Longitude must be between -180 and 180' }),
+  })
+
+  const { title, description, phone, latitude, longitude } = createGymBodySchema.parse(req.body)
+
+  const createGymUseCase = makeCreateGymUseCase()
+
+  await createGymUseCase.execute({ title, description, phone, latitude, longitude })
+
+  return res.status(201).send({ message: 'Gym has been created succesfuly' })
+
+}
+
