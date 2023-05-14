45cb8e7ccccf1e2ecafeb54516413cf93fd7404b
Gabriel Torres <gaabrieltt7@gmail.com>
2023-05-14 13:24:25 -0300

    refactor: if it is admin add true of false

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,16 +1,24 @@
+import { hash } from 'bcryptjs'
+import { prisma } from '@/lib/prisma'
 import { FastifyInstance } from 'fastify'
 import request from 'supertest'
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
-  await request(app.server).post('/users').send({
-    name: 'Example name',
-    email: 'any_email@mail.com',
-    password: 'any_password'
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+  isAdmin = false,
+) {
+  await prisma.user.create({
+    data: {
+      name: 'Any Name',
+      email: 'any_email@mail.com',
+      password_hash: await hash('any_password', 6),
+      role: isAdmin ? 'ADMIN' : 'MEMBER',
+    },
   })
 
   const authResponse = await request(app.server).post('/sessions').send({
     email: 'any_email@mail.com',
-    password: 'any_password'
+    password: 'any_password',
   })
 
   const { token } = authResponse.body
